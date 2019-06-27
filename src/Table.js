36429db02aa9610eaa1535d26a83ec9742c967e7
Brarian <b.namonywa@gmail.com>
2019-06-27 18:45:29 -0500

    connected Table component to redux

--- src/Table.js
+++ src/Table.js
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
 import "./Table.css";
-
+import { connect } from "react-redux";
+import { fetchData } from "./actions/index";
 class Table extends Component {
   //componentDidMoubt
+  componentDidMount() {
+    this.props.fetchData();
+  }
+
   //Render table data
+
+  renderHeadings() {
+    let headings = ["Name", "Email", "City", "Company"];
+    return headings.map((header, i) => {
+      return <th key={i}>{header}</th>;
+    });
+  }
+  renderTable() {
+    return this.props.data.map((user, i) => {
+      return (
+        <tr key={i}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>{user.address.city}</td>
+          <td>{user.company.name}</td>
+        </tr>
+      );
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header" />
-        {/* display table data  */}
-      </div>
+      <table className="main">
+        <thead>
+          <tr>{this.renderHeadings()}</tr>
+        </thead>
+        <tbody>{this.renderTable()}</tbody>
+      </table>
     );
   }
 }
 //mapstatetoprops
+const mapStateToProps = state => {
+  return {
+    data: state.data,
+    loading: state.loading,
+    error: state.error
+  };
+};
 //mapdispatchtoProps
+const matchDispatchToProps = dispatch => ({
+  fetchData: () => dispatch(fetchData())
+});
+
 //connect
-export default Table;
+export default connect(
+  mapStateToProps,
+  matchDispatchToProps
+)(Table);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Table from "./Table";
+import store from "./store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<Table />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Table />
+  </Provider>,
+  document.getElementById("root")
+);
