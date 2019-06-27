f9d6236412672dd9a86aa158ea10e4d0c27cc15e
Brarian <b.namonywa@gmail.com>
2019-06-27 17:38:48 -0500

    reducer tests defined

--- src/reducers /index.js
+++ src/reducers /index.js
@@ -1,3 +1,15 @@
 // import actions
+import {
+  FETCH_DATA_PENDING,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_ERROR
+} from "../actions/index";
+
 // set up initial state
+let initialState = {
+  error: null,
+  data: [],
+  loading: false
+};
+
 // root reducer
--- src/reducers /index.test.js
+++ src/reducers /index.test.js
@@ -1,6 +1,20 @@
+import {
+  FETCH_DATA_PENDING,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_ERROR
+} from "../actions/index";
+
 //test following actions:
 //1. no action
 //2. unknown action
 //3. pending
 //4. success
 //5. failure
+
+describe("rootReducer output", () => {
+  it("should return current state when given no action", () => {});
+  it("should return current state when given no action", () => {});
+  it("should return pending:true state when given FETCH_DATA_PENDING", () => {});
+  it("should return data when given FETCH_DATA_SUCCESS", () => {});
+  it("should return error:true state when given FETCH_DATA_ERROR", () => {});
+});
