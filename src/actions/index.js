1ee17e8826a5ec886ea3baeedce1ac5be398053e
Brarian <b.namonywa@gmail.com>
2019-06-27 17:31:17 -0500

    action tests passing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,38 @@
 //actions
-//1. fetch sucess
+//1. fetch success
 //2. Fetch data error
 //3. fetch data pending
+
+export const url = `https://jsonplaceholder.typicode.com/users`;
+
+export const FETCH_DATA_PENDING = "FETCH_DATA_PENDING";
+
+export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
+export const fetchDataSuccess = data => {
+  return {
+    type: FETCH_DATA_SUCCESS,
+    payload: data
+  };
+};
+
+export const FETCH_DATA_ERROR = "FETCH_DATA_ERROR";
+export const fetchDataError = error => {
+  return {
+    type: FETCH_DATA_ERROR,
+    error
+  };
+};
+
+export const fetchData = () => dispatch => {
+  return fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        return res.statusText("error fetching data");
+      }
+      return res.json();
+    })
+    .then(data => {
+      dispatch(fetchDataSuccess(data));
+    })
+    .catch(error => dispatch(fetchDataError(error)));
+};
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,2 +1,44 @@
 //test fetch sucess
 //test fetch error
+import {
+  url,
+  FETCH_DATA_PENDING,
+  FETCH_DATA_SUCCESS,
+  fetchDataSuccess,
+  FETCH_DATA_ERROR,
+  fetchDataError,
+  fetchData
+} from "./index";
+
+describe("fetch data", () => {
+  it("should dispatch fetchDataSuccess", () => {
+    const data = [];
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json() {
+          return data;
+        }
+      })
+    );
+
+    const dispatch = jest.fn();
+    return fetchData()(dispatch).then(() => {
+      expect(fetch).toHaveBeenCalledWith(url);
+      expect(dispatch).toHaveBeenCalledWith(fetchDataSuccess(data));
+    });
+  });
+
+  it("should return an error if the promise rejects", () => {
+    const error = {};
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(error);
+    });
+
+    const dispatch = jest.fn();
+    return fetchData()(dispatch).then(() => {
+      expect(fetch).toHaveBeenCalledWith(url);
+      expect(dispatch).toHaveBeenCalledWith(fetchDataError(error));
+    });
+  });
+});
